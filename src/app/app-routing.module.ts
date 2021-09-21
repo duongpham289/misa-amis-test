dfdbaf29f067ea1728bf6e47b5596e73b8660b80
duongpham289 <duongshin2000@gmail.com>
2021-09-21 09:13:48 +0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ManagementComponent } from './management/management.component';
+
+const routes: Routes = [
+  { path: '', component: ManagementComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
